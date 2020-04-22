f5156855607f5a7608b9d2e7d6c9545c8bee4ef5
maxikgreat <maximvasylenko228322@gmail.com>
2020-04-22 11:35:16 +0200

    All packages installed

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './scss/styles.scss';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app =
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>;
+
+ReactDOM.render(app, document.getElementById('root'));
 
 serviceWorker.unregister();
