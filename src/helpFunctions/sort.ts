bca57e90d8d9578f9381b5d3cdec11b805d162d1
maxikgreat <maximvasylenko228322@gmail.com>
2020-04-24 21:13:53 +0200

    Sorting added

--- src/helpFunctions/sort.ts
+++ src/helpFunctions/sort.ts
@@ -1,4 +1,4 @@
-import {OrderFull, OrderType, Quiz} from "../interfaces";
+import {Complexity, OrderFull, OrderType, Quiz} from "../interfaces";
 
 export function sort(order: OrderFull, collection: Quiz[]): Quiz[] {
     switch(order.type){
@@ -13,9 +13,24 @@ export function sort(order: OrderFull, collection: Quiz[]): Quiz[] {
             });
             return order.direction ? collection.reverse() : collection;
         case OrderType.complexity:
-            collection.sort((leftHand, rightHand) => {
-                return leftHand.complexity > rightHand.complexity ? -1 : -1
+            const collectionWithNumbers = collection.map((quiz: Quiz): {quiz: Quiz, value: number} => {
+                if(quiz.complexity === Complexity.easy) {
+                    return {quiz, value: 0}
+                } else if(quiz.complexity === Complexity.medium) {
+                    return {quiz, value: 1}
+                } else {
+                    return {quiz, value: 2}
+                }
+            })
+
+            collectionWithNumbers.sort((leftHand, rightHand): number => {
+                return leftHand.value > rightHand.value ? 1 : -1;
             });
-            return order.direction ? collection.reverse() : collection;
+
+            const readyCollection = collectionWithNumbers.map((quizItem: {quiz: Quiz, value: number}): Quiz => {
+                return quizItem.quiz
+            })
+
+            return order.direction ? readyCollection.reverse() : readyCollection;
     }
 }
